a3a4b2e242f64f5de3d4a1a2065afecb2e326a6e
samo1et <studio-sam@mail.ru>
2021-11-03 19:33:18 +0700

    Add Faq componnen

---
+++ src/components/Faq/index.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import ItemArticle from "./ItemArticle";
+
+const defaultState = [
+    {
+      id: 1,
+      title: "First article title",
+      about: "First article about",
+    },
+    {
+      id: 2,
+      title: "Second article title",
+      about: "Second article about",
+    },
+]
+
+export class Faq extends Component {
+  state = {
+    articles: defaultState,
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.articles.map((article) => (
+          <ItemArticle id={article.id} article={article} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Faq;
