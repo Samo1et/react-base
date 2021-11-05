169c630a4c795c8fe5f1bc0d5e909ba7db48d66b
samo1et <studio-sam@mail.ru>
2021-11-05 14:38:35 +0700

    add form style

--- src/components/Faq/Form.jsx
+++ src/components/Faq/Form.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import styles from './faq.module.scss'
 
 export class Form extends Component {
   state = {
@@ -7,42 +8,44 @@ export class Form extends Component {
   };
 
   handleChange = (e) => {
-        if (e.target.value) {
-            this.setState({[e.target.name]: e.target.value})
-        }
+    if (e.target.value) {
+      this.setState({ [e.target.name]: e.target.value });
+    }
   };
 
   handleSubmitForm = (e) => {
-      e.preventDefault();
-      const { title, about } = this.state;
-      if (title && about) {
-          this.props.addArticle({title, about})
-      }
+    e.preventDefault();
+    const { title, about } = this.state;
+
+    if (title && about) {
+      this.props.addArticle({ title, about });
+    }
   };
 
   render() {
     return (
       <div>
         <form onSubmit={this.handleSubmitForm}>
-          <label>
-            <input
-              onChange={this.handleChange}
-              type="text"
-              name="title"
-              placeholder="Добавьте заголовок"
-            />
-          </label>
-          <br />
-          <label>
-            <input
-              onChange={this.handleChange}
-              type="text"
-              name="about"
-              placeholder="Добавьте текст"
-            />
-          </label>
-          <br />
-          <button type="submit">Добавить</button>
+          <fieldset>
+            <legend>Форма</legend>
+            <label className={styles.label}>
+              <input
+                onChange={this.handleChange}
+                type="text"
+                name="title"
+                placeholder="Добавьте заголовок"
+              />
+            </label>
+            <label className={styles.label}>
+              <input
+                onChange={this.handleChange}
+                type="text"
+                name="about"
+                placeholder="Добавьте текст"
+              />
+            </label>
+            <button type="submit">Добавить</button>
+          </fieldset>
         </form>
       </div>
     );
